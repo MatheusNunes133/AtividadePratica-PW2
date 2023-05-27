f611c3e9185eef071ce52f3c9934cd23c7325eea
Matheus Nunes Miguel <matheusnunesmiguel6@gmail.com>
2023-05-26 21:58:05 -0300

    feat: Adiciona componente Post na pagina principal

--- AtividadeExtra/src/App.tsx
+++ AtividadeExtra/src/App.tsx
@@ -1,5 +1,6 @@
 import Header from './components/Header'
 import UserPerfil from './components/UserPerfil'
+import Post from './components/Post';
 import "./App.css";
 
 function App() {
@@ -12,7 +13,14 @@ function App() {
         <UserPerfil />
 
         <main>
-          <h1>Aqui fica o Post</h1>
+          <Post userImage='https://avatars.githubusercontent.com/u/80282447?v=4'
+            username='Matheus Nunes'
+            profession='Back-end Developer'
+            desc='Imagem com perfil'
+            postTitle='Falaaa Galeraa!!'
+            post='Acabei de realizar a atividade extra da disciplina de PW2!'
+            anchors='#typescript #React #vite'
+          />
         </main>
       </div>
 
